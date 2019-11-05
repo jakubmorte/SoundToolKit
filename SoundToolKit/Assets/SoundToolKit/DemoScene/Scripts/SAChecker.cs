c24607b592e12b2c259187934a6fef36baa2d344
admin <jakubmorte@gmail.com>
2019-11-05 16:53:41 +0100

    Tutorial pt.1&2

---
+++ SoundToolKit/Assets/SoundToolKit/DemoScene/Scripts/SAChecker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SAChecker : MonoBehaviour
+{
+    void Start()
+    {
+        Component[] components = gameObject.GetComponents<Component>();
+
+        foreach (var component in components)
+        {
+            if (component == null)
+            {
+                Debug.LogWarning("[SoundToolKit] Characters package missing in the project. Please import StandardAssets/Characters package in order for the demo scene to work.");
+            }
+        }
+    }
+}
---
+++ SoundToolKit/Assets/SoundToolKit/Scripts/MovingSource.cs
@@ -0,0 +1,60 @@
+﻿// \author: Michal Majewski
+// \date: 13.03.2019
+
+using UnityEngine;
+
+/// <summary>
+/// A behaviour that moves the object from Point1 to Point2 and back at a given speed;
+/// </summary>
+public class MovingSource : MonoBehaviour
+{
+    #region public properties
+
+    [SerializeField]
+    public Vector3 Point1 = new Vector3(0, 0, 0);
+
+    [SerializeField]
+    public Vector3 Point2 = new Vector3(1, 1, 1);
+
+    [SerializeField]
+    public float speed = 0.1f;
+
+    #endregion
+
+    #region private methods
+
+    private void Awake()
+    {
+        gameObject.transform.position = Point1;
+    }
+
+    private void Update()
+    {
+        if (!m_reversedMovment)
+        {
+            MoveTowards(Point2);
+        }
+        else
+        {
+            MoveTowards(Point1);
+        }
+    }
+
+    private void MoveTowards(Vector3 point)
+    {
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, point, speed);
+
+        if (Vector3.Distance(gameObject.transform.position, point) < 0.001f)
+        {
+            m_reversedMovment = !m_reversedMovment;
+        }
+    }
+
+    #endregion
+
+    #region private members
+
+    private bool m_reversedMovment = false;
+
+    #endregion
+}
